5ae2b63f0e265e27d70a10a5a29fe4ac057210cc
redasalmi <reda.salmi.elt@gmail.com>
2020-11-11 19:52:41 +0100

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+const Index = () => {
+  return (
+    <div>
+      <Head>
+        <title>Home Page</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <main>
+        <h1>Hello Next JS</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Index;
