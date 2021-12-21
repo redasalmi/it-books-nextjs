e55d6fb98dd5a9b6693a9f2ab553ae17f32c250e
redasalmi <reda.salmi.elt@gmail.com>
2021-12-21 17:15:02 +0100

    replaced google font with @fontsource/poppins

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,8 @@ import Welcome from '../components/Welcome';
 import Footer from '../components/Footer';
 
 import fetchBooks from '../utils/fetchBooks';
+import '@fontsource/poppins/400.css';
+import '@fontsource/poppins/700.css';
 import '../styles/index.scss';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,19 +15,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang='en'>
-        <Head>
-          <link rel='preconnect' href='https://fonts.googleapis.com' />
-          <link
-            rel='preconnect'
-            href='https://fonts.gstatic.com'
-            crossOrigin='true'
-          />
-          <link
-            href='https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap'
-            rel='stylesheet'
-          />
-        </Head>
-
+        <Head />
         <body>
           <Main />
           <NextScript />
